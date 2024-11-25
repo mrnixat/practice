1f7fbb0cc3d840ef0feeec43eedf01f96fe5dce1
Nikos Tsakiridis <nikotsak200@gmail.com>
2024-11-25 12:50:00 -0600

    python methods

---
+++ Python3/Introduction to Python Classes/Methods/script.py
@@ -0,0 +1,3 @@
+class Rules:
+  def washing_brushes(self): # self is very important here (never knew what this was until now)
+    return "Point bristles towards the basin while washing your brushes."
