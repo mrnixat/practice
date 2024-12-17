55179bc81d95bb32664694378ee0545fd89a0f73
Nikos Tsakiridis <nikotsak200@gmail.com>
2024-12-16 19:54:56 -0600

    completed contains duplicate

---
+++ LeetCode/Contains Duplicate/solution.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+
+       unordered_map<int, int> hash;
+
+        for (int num : nums)
+        {
+            auto it = hash.find(num);
+            if (it == hash.end())
+                hash[num] = 0;
+            else return true;
+        }
+        return false;
+
+/*
+    set<int> s;
+    vector<int> new_vec;
+
+    for (int num : nums)
+    {
+        s.insert(num);
+        new_vec.push_back(num);
+        if (s.size() < new_vec.size()) return true;
+    }
+
+    return false; */
+    }
+};
