10a0e03f2c8b5519b7a15dd2aec397569e98db6f
Nikos Tsakiridis <nikotsak200@gmail.com>
2024-12-31 19:37:46 -0600

    more practice

--- Python3/test.py
+++ Python3/test.py
@@ -1,9 +1,11 @@
-def triangle(n):
-    tempo = ""
-    for i in range(n):
-        tempo+= '*'
-        print(tempo)
+nums=[0,3,2,5,4,6,1,1]
+sorted_nums = sorted(set(nums))
+removed = 0
 
-print()
-triangle(4)
-print()
+for i in sorted_nums:
+    print("original:", sorted_nums)
+    removed = i
+    sorted_nums.remove(i)
+
+    print("This is the list now:", sorted_nums)
+    print("and this is the removed now:", removed)
